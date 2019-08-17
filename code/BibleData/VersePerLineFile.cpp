db486722d5a7e27473914d962cbdeadc0968aa97
jpike <jpikegames@gmail.com>
2019-08-17 11:43:50 -0400

    Adding comments to VersePerLineFile.

--- code/BibleData/VersePerLineFile.cpp
+++ code/BibleData/VersePerLineFile.cpp
@@ -4,12 +4,16 @@
 
 namespace BIBLE_DATA
 {
+    /// Attempts to parse the file at the specified path.
+    /// @param[in]  filepath - The path to the file to parse.
+    /// @return The parsed verse-per-line file, if successful; std::nullopt otherwise.
     std::optional<VersePerLineFile> VersePerLineFile::Parse(const std::filesystem::path& filepath)
     {
         // MAKE SURE THE FILE CAN BE OPENED.
         std::ifstream verse_per_line_file(filepath);
         if (!verse_per_line_file.is_open())
         {
+            // INDICATE THAT THE FILE FAILED TO BE PARSED.
             return std::nullopt;
         }
 
@@ -19,26 +23,38 @@ namespace BIBLE_DATA
         std::string line;
         while (std::getline(verse_per_line_file, line))
         {
-            // PARSE OUT THE COMPONENTS OF THE LINE.
             BibleVerse verse;
 
+            // PARSE OUT THE BOOK NAME.
+            // The format of each line is Book|Chapter#|Verse#| VerseText~
+            constexpr char COMPONENT_SEPARATOR = '|';
             std::istringstream line_text(line);
-            std::getline(line_text, verse.Book, '|');
+            std::getline(line_text, verse.Book, COMPONENT_SEPARATOR);
 
+            // PARSE OUT THE CHAPTER NUMBER.
             std::string chapter_number;
-            std::getline(line_text, chapter_number, '|');
+            std::getline(line_text, chapter_number, COMPONENT_SEPARATOR);
             verse.ChapterNumber = std::stoi(chapter_number);
 
+            // PARSE OUT THE VERSE NUMBER.
             std::string verse_number;
-            std::getline(line_text, verse_number, '|');
+            std::getline(line_text, verse_number, COMPONENT_SEPARATOR);
             verse.VerseNumber = std::stoi(verse_number);
 
-            std::getline(line_text, verse.Text, '~');
-            verse.Text.erase(0, 1);
+            // PARSE OUT THE VERSE TEXT.
+            constexpr char LINE_TERMINATOR = '~';
+            std::getline(line_text, verse.Text, LINE_TERMINATOR);
 
+            // A single space character is at the beginning of this last component.
+            constexpr std::size_t START_INDEX = 0;
+            constexpr std::size_t SPACE_CHARACTER_COUNT = 1;
+            verse.Text.erase(START_INDEX, SPACE_CHARACTER_COUNT);
+
+            // STORE THE PARSED VERSE.
             verses.push_back(verse);
         }
 
+        // RETURN THE PARSED FILE.
         return VersePerLineFile{ .Verses = verses };
     }
 }
--- code/BibleData/VersePerLineFile.h
+++ code/BibleData/VersePerLineFile.h
@@ -7,11 +7,16 @@
 
 namespace BIBLE_DATA
 {
+    /// A simple text file that contains one Bible verse per line.
+    /// This specific format is based on the KJV data from http://sacred-texts.com/bib/osrc/index.htm.
     class VersePerLineFile
     {
     public:
+        // PARSING.
         static std::optional<VersePerLineFile> Parse(const std::filesystem::path& filepath);
 
+        /// PUBLIC MEMBER VARIABLES FOR EASY ACCESS.
+        /// The verses parsed from the file.
         std::vector<BibleVerse> Verses = {};
     };
 }
